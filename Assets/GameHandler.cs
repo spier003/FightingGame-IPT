bb802aba56c2a5b7b61e40e67cc4b201112dbc53
EdrickDefeo <86871115+spier003@users.noreply.github.com>
2022-09-30 03:51:04 +0800

    setname&hp
    setname&hp

--- Assets/GameHandler.cs
+++ Assets/GameHandler.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
+    //PLAYERS HP
+    public float P1HP;
+    public float P2HP;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +29,26 @@ public class GameHandler : MonoBehaviour
     public void QuitGame(){
         Debug.Log("Quit");
         Application.Quit();
+    }
+    // Set HP
+    public void SetHP50(){
+        P1HP = 50.0f;
+        P2HP = 50.0f;
+        Debug.Log("Player 1 HP: " + P1HP);
+        Debug.Log("Player 2 HP: " + P2HP);
+    }
+    public void SetHP100(){
+        P1HP = 100.0f;
+        P2HP = 100.0f;
+
+        Debug.Log("Player 1 HP: " + P1HP);
+        Debug.Log("Player 2 HP: " + P2HP);
+    }
+    public void SetHP150(){
+        P1HP = 150.0f;
+        P2HP = 150.0f;
+
+        Debug.Log("Player 1 HP: " + P1HP);
+        Debug.Log("Player 2 HP: " + P2HP);
     }
 }
